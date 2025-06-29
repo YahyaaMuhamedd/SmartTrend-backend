a606227e73a7296e98335dce1d3e839d573d4614
MOOS <144733181+YahyaaMuhamedd@users.noreply.github.com>
2025-06-29 19:29:22 +0300

    remove moderator to add company

--- DataBase/dbConnection.js
+++ DataBase/dbConnection.js
@@ -26,13 +26,13 @@ import mongoose from "mongoose";
 
 
 // & Data Base Online Connection By Atlas :
-export const dbConnection = async ()=>{
-   await mongoose.connect(process.env.URL_CONNECTION_DB_ONLINE_ATLAS).then(()=>{
-      console.log("dbConnect Online ...." );
-   }).catch((error)=>{
-      console.log("Fail dbConnection Online ! " );
+export const dbConnection = async () => {
+   await mongoose.connect(process.env.URL_CONNECTION_DB_ONLINE_ATLAS).then(() => {
+      console.log("dbConnect Online ....");
+   }).catch((error) => {
+      console.log("Fail dbConnection Online ! ", error);
    })
-} ;
+};
 
 
 
--- src/modules/company/company.routes.js
+++ src/modules/company/company.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import * as companyControl from "./company.controller.js";
 import { multerLocal, validExtension } from "../../services/multer.Local.js";
-import {validation} from "../../middleWare/validation.js";
-import { addCompanyVal  , changeCoverVal, paramsIdVal , updateCompanyVal } from "./company.validate.js";
+import { validation } from "../../middleWare/validation.js";
+import { addCompanyVal, changeCoverVal, paramsIdVal, updateCompanyVal } from "./company.validate.js";
 import { protectedRoutes } from "../../middleWare/authentication.js";
 import { authorize } from "../../middleWare/authorization.js";
 import { ROLES } from "../../utilities/enums.js";
@@ -10,59 +10,59 @@ import { ROLES } from "../../utilities/enums.js";
 
 
 
-const router = Router() ;
+const router = Router();
 //^=========================== Get All Companies, Add Company ==================================
-   router.route("/")
-      .get( companyControl.getAllCompanies)
-      // .get(protectedRoutes , authorize(ROLES.ADMIN , ROLES.MODERATOR) , companyControl.getAllCompanies)
-      .post(protectedRoutes ,  
-         authorize(ROLES.ADMIN , ROLES.MODERATOR) , 
-         multerLocal(validExtension.image , "company").single("file"), 
-         validation(addCompanyVal) ,
-         companyControl.addCompany)
+router.route("/")
+   .get(companyControl.getAllCompanies)
+   // .get(protectedRoutes , authorize(ROLES.ADMIN , ROLES.MODERATOR) , companyControl.getAllCompanies)
+   .post(protectedRoutes,
+      authorize(ROLES.ADMIN),
+      multerLocal(validExtension.image, "company").single("file"),
+      validation(addCompanyVal),
+      companyControl.addCompany)
 
 
 
 
 //^=========================== Get Company Count dashboard =====================================
-   router.route("/getCompanyCount")
-      // .get( companyControl.getCompanyCount)
-      .get(protectedRoutes , authorize(ROLES.ADMIN , ROLES.MODERATOR) , companyControl.getCompanyCount)
+router.route("/getCompanyCount")
+   // .get( companyControl.getCompanyCount)
+   .get(protectedRoutes, authorize(ROLES.ADMIN, ROLES.MODERATOR), companyControl.getCompanyCount)
 
-   
+
 
 //^=========================== Get Single Company, Update, Delete, Change Image =================
-   router.route("/:id")
-      // .get( validation(paramsIdVal)  , companyControl.getSingleCompany)
-      .get(protectedRoutes ,  
-         authorize(ROLES.ADMIN , ROLES.MODERATOR  , ROLES.USER) , 
-         validation(paramsIdVal)  , 
-         companyControl.getSingleCompany)
+router.route("/:id")
+   // .get( validation(paramsIdVal)  , companyControl.getSingleCompany)
+   .get(protectedRoutes,
+      authorize(ROLES.ADMIN, ROLES.MODERATOR, ROLES.USER),
+      validation(paramsIdVal),
+      companyControl.getSingleCompany)
 
 
 
-      // .put(validation(updateCompanyVal) ,  companyControl.updateCompany)
-      .put(protectedRoutes , 
-         authorize(ROLES.ADMIN , ROLES.MODERATOR) , 
-         validation(updateCompanyVal) ,  
-         companyControl.updateCompany)
-      
-
-
-      // .patch(multerLocal(validExtension.image , "company").single("image") , validation(changeCoverVal) ,  companyControl.changeImgCover) 
-      .patch(protectedRoutes , 
-         authorize(ROLES.ADMIN , ROLES.MODERATOR) ,  
-         multerLocal(validExtension.image , "company").single("file") ,  
-         validation(changeCoverVal) ,
-         companyControl.changeImgCover) 
+   // .put(validation(updateCompanyVal) ,  companyControl.updateCompany)
+   .put(protectedRoutes,
+      authorize(ROLES.ADMIN, ROLES.MODERATOR),
+      validation(updateCompanyVal),
+      companyControl.updateCompany)
 
 
 
-      // .delete( validation(paramsIdVal)  , companyControl.deleteCompany)
-      .delete(protectedRoutes , 
-         authorize(ROLES.ADMIN , ROLES.MODERATOR) , 
-         validation(paramsIdVal)  , 
-         companyControl.deleteCompany)
-      
+   // .patch(multerLocal(validExtension.image , "company").single("image") , validation(changeCoverVal) ,  companyControl.changeImgCover) 
+   .patch(protectedRoutes,
+      authorize(ROLES.ADMIN, ROLES.MODERATOR),
+      multerLocal(validExtension.image, "company").single("file"),
+      validation(changeCoverVal),
+      companyControl.changeImgCover)
 
-export default router ;
+
+
+   // .delete( validation(paramsIdVal)  , companyControl.deleteCompany)
+   .delete(protectedRoutes,
+      authorize(ROLES.ADMIN, ROLES.MODERATOR),
+      validation(paramsIdVal),
+      companyControl.deleteCompany)
+
+
+export default router;
