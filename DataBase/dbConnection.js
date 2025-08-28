862ecdffb047e6b27e6e242bce6c2d38e1b435d9
Eng-MahmoudOthman <mahmoud_othman20@hotmail.com>
2025-08-28 12:43:03 +0300

    Create Api Payment Success , Failed and webHook

--- DataBase/dbConnection.js
+++ DataBase/dbConnection.js
@@ -35,6 +35,8 @@ export const dbConnection = async ()=>{
    await mongoose.connect(process.env.URL_CONNECTION_DB_ONLINE_ATLAS).then(()=>{
       console.log("dbConnect Online ...." );
    }).catch((error)=>{
+      console.log(error);
+      
       console.log("Fail dbConnection Online ! " );
    })
 } ;
